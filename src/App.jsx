fadf60b36d1bdfef9f4abecc410cbcb2a66223b9
Sebastian Manco G <sebasmg1393@gmail.com>
2025-05-03 14:03:27 -0500

    sebastian manco: add more reusability in button.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import { helloFromUtils, ohtherHi } from './utils/helpers';
 import styled from 'styled-components';
 import GlobalStyles from './styles/GlobalStlyes';
 import Button from './ui/Button';
 import Input from './ui/Input';
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+import Heading from './ui/Heading';
+import Row from './ui/Row';
 
 const StyledApp = styled.main`
   background-color: tomato;
@@ -21,17 +17,37 @@ function App() {
   helloFromUtils();
   ohtherHi();
 
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
       <GlobalStyles />
-
       <StyledApp>
-        <H1>Hello from biggest proyect.</H1>
-        <Button onClick={() => alert('Hi button bby ')}>Check in</Button>
-        <Button onClick={() => alert('Hi button bby ')}>Check out</Button>
-        <Input type="number" placeholder="Number of guests"></Input>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Hello from biggest proyect.</Heading>
+
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert('Hi button bby ')}>Check in</Button>
+              <Button
+                variations="danger"
+                onClick={() => alert('Hi button bby ')}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h2">Form</Heading>
+
+            <div>
+              <Input type="number" placeholder="Number of guests"></Input>
+              <Input type="number" placeholder="Number of guests"></Input>
+            </div>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
