d9213cab150a14fcf67508165490913b15c06e27
Sebastian Manco G <sebasmg1393@gmail.com>
2025-05-03 15:19:57 -0500

    sebastian manco pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,18 @@
-// import { useState } from 'react';
-import { helloFromUtils, ohtherHi } from './utils/helpers';
-import styled from 'styled-components';
-import GlobalStyles from './styles/GlobalStlyes';
-import Button from './ui/Button';
-import Input from './ui/Input';
-import Heading from './ui/Heading';
-import Row from './ui/Row';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-const StyledApp = styled.main`
-  background-color: tomato;
-  padding: 20px;
-  border: 8px solid black;
-`;
+import Dashboard from './pages/Dashboard';
 
 function App() {
-  helloFromUtils();
-  ohtherHi();
-
-  // const [count, setCount] = useState(0);
-
   return (
-    <>
-      <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h1">Hello from biggest proyect.</Heading>
-
-            <div>
-              <Heading as="h2">Check in and out</Heading>
-              <Button onClick={() => alert('Hi button bby ')}>Check in</Button>
-              <Button
-                variations="danger"
-                onClick={() => alert('Hi button bby ')}
-              >
-                Check out
-              </Button>
-            </div>
-          </Row>
-
-          <Row>
-            <Heading as="h2">Form</Heading>
-
-            <div>
-              <Input type="number" placeholder="Number of guests"></Input>
-              <Input type="number" placeholder="Number of guests"></Input>
-            </div>
-          </Row>
-        </Row>
-      </StyledApp>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="dashborad" element={<Dashboard />} />
+        <Route path="" element={<Dashboard />} />
+        <Route path="" element={<Dashboard />} />
+        <Route path="" element={<Dashboard />} />
+        <Route path="" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
